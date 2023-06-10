9e0c94cf7eb2455a668fa5996c27e0ab97383804
mtt-ramirez <112211254+mtt-ramirez@users.noreply.github.com>
2023-06-10 18:23:33 -0500

    Update question.sh

--- question.sh
+++ question.sh
@@ -40,13 +40,5 @@
 #  2014-09-01,A,3,100.4
 #
 #  >>> Escriba su codigo a partir de este punto <<<
-#
-#sed -E 's|([0-9]{2})/([0-9]{2})/([0-9]{2})|20\3-\2-\1|g ; s|([0-9]{1})/([0-9]{1})/(20[0-9]{2})|\3-0\2-0\1|; s|[a-z]|\U&|g; s|,|.|g; s|;|,|g'
-
-#sed -e 's|([0-9]{2})/([0-9]{2})/([0-9]{2})|20\3-\2-\1|g' \ 
-#    -e 's|([0-9]{1})/([0-9]{1})/(20[0-9]{2})|\3-0\2-0\1|' \
-#    -e 'y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/' \
-#    -e 's|,|.|g; s|;|,|g; s|,N|,\\N|g; s|,,|,\\N,|; 5 s|$|\\N|; 7 s|$|\\N|' \
-#    -e data.csv > output.csv 
 
 sed -E 's|([0-9]{2})/([0-9]{2})/([0-9]{2})|20\3-\2-\1|g ; s|([0-9]{1})/([0-9]{1})/(20[0-9]{2})|\3-0\2-0\1|; y/abcdefghijklmnopqrstuvwxyz/ABCDEFGHIJKLMNOPQRSTUVWXYZ/; s|,|.|g; s|;|,|g; s|,N|,\\N|g; s|,,|,\\N,|; 5 s|$|\\N|; 7 s|$|\\N|' data.csv > output.csv
